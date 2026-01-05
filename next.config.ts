cb9f1975dd70b8553934898f22a31f9afffe0f64
TyJam <djkoatz@gmail.com>
2026-01-05 16:35:39 -0500

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // <--- ADD THIS LINE
+  images: {
+    unoptimized: true, // <--- AND THIS LINE (Required for static export)
+  },
 };
 
 export default nextConfig;
